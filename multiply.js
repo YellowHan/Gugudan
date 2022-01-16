04ac374b079cbbccc15e0e89ba1229dd8c291d94
YellowHan <89592727+YellowHan@users.noreply.github.com>
2022-01-16 17:05:37 +0900

    Add files via upload

--- multiply.js
+++ multiply.js
@@ -10,7 +10,7 @@ document.addEventListener('click', e => {
     question.innerHTML = "";
     for(let i = 1; i < 10; i++) {
       random1[i] = mul == "mul2"? Math.floor((Math.random()*90)+10) : mul == "mul3"? Math.floor((Math.random()*900)+100) : "";
-      random2[i] = mul == "mul2"? Math.floor((Math.random()*10)) : mul == "mul3"? Math.floor((Math.random()*90)+10) : "";
+      random2[i] = mul == "mul2"? Math.floor((Math.random()*10)+1) : mul == "mul3"? Math.floor((Math.random()*90)+10) : "";
       question.innerHTML += `${random1[i]} * ${random2[i]} = <input id='mul${i}' type='text' value='' /><a id='answer${i}'></a><br>`
     }
     document.getElementById('check').innerHTML = "<input type='button' value='정답체크' />";
@@ -19,7 +19,6 @@ document.addEventListener('click', e => {
     if(mul == "mul2" || mul == "mul3") {
       for(let i = 1; i < 10; i++) {
         document.getElementById(`mul${i}`).value == random1[i] * random2[i]? document.getElementById(`answer${i}`).innerText = "O" : document.getElementById(`answer${i}`).innerText = "X";
-        console.log(`random1 : ${random1[i]} * random2 : ${random2[i]} = document.getElementById("mul${i}").value`)
       }
     }
   }
